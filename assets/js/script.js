efdb05136ed3b5f9d6d6eeb391139e6960f266d0
nichojohnson84 <nickcjohnson84@outlook.com>
2022-02-06 14:42:51 -0700

    doOver

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,53 @@
 //addeventlistener for search button bring weather for city entered in the text box
-var button = document.getElementById ("btn")
+var button = document.getElementById("btn");
 var cityResponse;
 var tempResponse;
 var humidityResponse;
 var uvIndexResponse;
 
-
-button.addEventListener ('click', getWeather)
-function getWeather (event) {
-    event.preventDefault ()
-    var localSearch = event.target.previousSibling.previousSibling.value
-    var geoSearch = "http://api.openweathermap.org/geo/1.0/direct?q="+localSearch+"&appid=48454c83470277fd99afd700c55b471f"
-    fetch (geoSearch)
-    .then (function (data){
-        return data.json ()
+button.addEventListener("click", getWeather);
+function getWeather(event) {
+  event.preventDefault();
+  var localSearch = event.target.previousSibling.previousSibling.value;
+  var geoSearch =
+    "http://api.openweathermap.org/geo/1.0/direct?q=" +
+    localSearch +
+    "&appid=48454c83470277fd99afd700c55b471f";
+  fetch(geoSearch)
+    .then(function (data) {
+      return data.json();
     })
-    .then (function (response){
-        var lat = response[0].lat
-        var lon = response[0].lon
-        var oneCall = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&units=imperial&appid=48454c83470277fd99afd700c55b471f"
-        cityResponse = response
-        console.log (response)
-        
-        fetch (oneCall)
-        .then (function (data){
-            return data.json ()
-        })
-        .then (function (response){
-        console.log (response)
-        displayCurrentWeather (response,cityResponse)
-        })
-        .then (function (response){
-            console.log (response)
-            displayCurrentTemp (response,tempResponse)
-        })
+    .then(function (response) {
+      var lat = response[0].lat;
+      var lon = response[0].lon;
+      var oneCall = fetch ("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=48454c83470277fd99afd700c55b471f");
     })
+       
+        .then(function (response) {
+            console.log(response)
+          return response.json();
+        })
+        /*
+        .then(function (response) {
+          console.log(response);
+          displayCurrentWeather(response);
+        })
+        .then(function (response) {
+          displayCurrentTemp(response, tempResponse);
+          console.log(tempResponse, response);
+        });*/
 }
-function displayCurrentWeather (weather,currentCity) {
-    console.log (weather)
-   
-    var city = document.getElementById ("city")
-    var temp = document.getElementById ("temp")
-    var humidity = document.getElementById ("humidity")
-    var uvIndex = document.getElementById ("uvIndex")
-    city.textContent = currentCity[0].name
-    temp.value = currentTemp[0].value
-    
+
+function displayCurrentWeather(weather, currentCity) {
+  console.log(weather);
+
+  var weatherIcon = document.getElementById("weatherIcon");
+  var city = document.getElementById("city");
+  var temp = document.getElementById("temp");
+  var humidity = document.getElementById("humidity");
+  var uvIndex = document.getElementById("uvIndex");
+  city.textContent = currentCity[0].name;
+  temp.textContent = weather.current.temp + "&deg;";
 }
 /*snow = <p>Unicode:</p>
 <i style='font-size:24px' class='fas'>&#xf2dc;</i>
@@ -69,19 +71,5 @@ or far fa-sun
 Rain=<p>Unicode:</p>
 <i style='font-size:24px' class='fas'>&#xf73d;</i>
 or fas fa-cloud-rain*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 //
